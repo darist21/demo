753019b65b20d95a57f93d8945d6249d2d18334f
darist21 <31358217+darist21@users.noreply.github.com>
2017-09-02 09:48:04 +0300

    Initial version

---
+++ bin/CMakeFiles/view.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/view.dir/home/ari/projects/demo/source/view.cpp.o"
+  "view.pdb"
+  "view"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/view.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
